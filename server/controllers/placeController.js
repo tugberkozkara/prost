36503025028a135a71ec3cdae355787bb4bb456b
tugberkozkara <tugberkozkara@gmail.com>
2022-12-05 00:35:01 +0300

    Add client communication

--- server/controllers/placeController.js
+++ server/controllers/placeController.js
@@ -13,13 +13,11 @@ router.get('/', async(req, res) => {
     res.status(200).json(allPlaces);
 });
 
-router.post('/create', jsonParser, (req, res) => {
+router.post('/', jsonParser, (req, res) => {
     console.log(req.body);
     if(!req.body._id){
         createPlace(req, res);
     }
-    updatePlace(req, res);
-    // Update is not working!
 })
 
 
@@ -34,30 +32,12 @@ function createPlace(req, res){
         tags: req.body.tags
     })
     place.save((err, doc) => {
-        if(err) res.status(400);
+        if(err){
+            res.status(400);
+        } 
         res.status(201).json("Created successfully!");
     });
 }
-
-//TO:DO - Solve Here
-function updatePlace(req, res){
-    filter = {
-        _id : req.body._id
-    }
-    update = {
-        name: req.body.name,
-        category: req.body.category,
-        location: req.body.location,
-        price: req.body.price,
-        menu: req.body.menu
-    }
-    Place.findOneAndUpdate(filter, update, {
-        new: true,
-        upsert: true
-      });
-    res.status(200).json("Updated successfully!");
-}
-
 
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const app = express();
 const userController = require('./controllers/userController');
 const placeController = require('./controllers/placeController');
+const cors = require('cors');
 require('./config/db')
 
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hellor woldr!");
     return res.status(200)
 });
 
-app.listen(8080, () =>{
-    console.log(`Server is running on port 8080`);
+app.listen(5000, () =>{
+    console.log(`Server is running on port 5000`);
 });
 
 app.use('/users', userController);
