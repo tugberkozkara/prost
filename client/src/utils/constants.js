55354a0302ad839806a675d6b7e5c0fa528f5b6a
tugberkozkara <tugberkozkara@gmail.com>
2022-12-22 23:12:12 +0300

    Add tags endpoint to request

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,3 +2,4 @@ export const BASE_URL = "http://127.0.0.1:5000";
 export const API_URL_PLACES_GET_POST = BASE_URL + "/places";
 export const API_URL_LOGIN =  BASE_URL + "/users/login";
 export const API_URL_REGISTER =  BASE_URL + "/users/register";
+export const API_URL_TAGS_GET_POST = BASE_URL + "/tags";
