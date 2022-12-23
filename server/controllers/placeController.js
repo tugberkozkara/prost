a6e8019226e1157f63a974a2000c10f973482d94
tugberkozkara <tugberkozkara@gmail.com>
2022-12-23 18:06:18 +0300

    Add filtering by query params user or tag

--- server/controllers/placeController.js
+++ server/controllers/placeController.js
@@ -1,11 +1,14 @@
 import Place from '../models/place.js';
-import Tag from '../models/tag.js';
 
 export default class PlaceController{
     
     static getAllPlaces = async (request, response) => {
+
+        const tagParams = request.query.tags;
+        const userParam = request.query.user;
+
         try {
-            const allPlaces = await Place.find().populate([
+            let allPlaces = await Place.find().populate([
                 {
                     path: 'createdBy',
                     select: 'username'
@@ -15,6 +18,17 @@ export default class PlaceController{
                     select: 'name'
                 }
             ]);
+
+            if(tagParams !== undefined){
+                const tagQueryArray = tagParams.split(',').map(e => e.trim());
+                let checker = (arr, target) => target.every(element => arr.includes(element));
+                allPlaces = allPlaces.filter(place => checker(place.tags.map(tag => tag.name), tagQueryArray));
+            }
+
+            if(userParam !== undefined){
+                allPlaces = allPlaces.filter(place => place.createdBy.username == userParam);
+            }
+            
             response.status(200).json(allPlaces);
         } catch (error) {
             response.status(404).json({
@@ -53,7 +67,7 @@ export default class PlaceController{
     }
 
 
-    static getPlace = async (request, response) => {
+    static getPlaceById = async (request, response) => {
         const { placeId } = request.params;
         const place = await Place.findOne({_id: placeId});
         
@@ -62,7 +76,6 @@ export default class PlaceController{
                 message: "Not found!",
             });
         }
-
         return response.status(200).json(place);
     }
 }
