846f57f6026dad96b6ec4f93fff00a4d22419528
tugberkozkara <tugberkozkara@gmail.com>
2023-01-18 19:42:24 +0300

    Add filtering by tags

---
+++ client/src/components/Filter.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react"
+import { API_URL_TAGS_GET_POST } from '../utils/constants';
+import axios from 'axios';
+
+
+const Filter = ({places, setFiltered}) => {
+    const [tags, setTags] = useState([]);
+    const selectedTags = [];
+    const [activeTags, setActiveTags] = useState(selectedTags);
+    
+    const getTags = async () => {
+        try {
+          const allTags = await axios.get(API_URL_TAGS_GET_POST);
+          setTags(allTags.data);
+        } catch (error) {
+          console.log(error);
+        }
+    }
+
+    useEffect(() => { 
+        getTags();
+      }, []);
+
+    useEffect(() => {
+        if(activeTags.length === 0){
+            setFiltered(places);
+            return;
+        }
+        const filtered = places.filter((place) => place.tags.some(tag => activeTags.includes(tag._id)));
+        setFiltered(filtered);
+    }, [activeTags]);
+
+    const tagHandle = (event) => {
+        if(activeTags.includes(event.target.value)){
+            setActiveTags(activeTags.filter(id => id !== event.target.value));
+            event.target.className = "btn btn-outline-secondary mx-1";
+        }
+        else{
+            setActiveTags((prevTags) => [...prevTags, event.target.value]);
+            event.target.className = "btn btn-secondary mx-1";
+        }
+    }
+
+  return (
+    <div className="btn-group d-flex justify-content-center mb-2" role="group" aria-label="Basic example">
+        {
+            tags.map((tag, i) =>
+                <div key={i} >
+                    <button value={tag._id} onClick={tagHandle} className="btn btn-outline-secondary mx-1">{tag.name}</button>
+                </div>)
+        }
+    </div>
+  )
+}
+
+export default Filter
--- client/src/pages/ListPlaces.js
+++ client/src/pages/ListPlaces.js
@@ -1,34 +1,40 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import PlaceCard from '../components/PlaceCard';
+import Filter from '../components/Filter';
 import { API_URL_PLACES_GET_POST } from '../utils/constants';
 
 export default function ListPlaces(){
     
     const [places, setPlaces] = useState([]);
+    const [filtered, setFiltered] = useState([]);
 
     const getPlaces = async () => {
         try {
           const allPlaces = await axios.get(API_URL_PLACES_GET_POST);
           setPlaces(allPlaces.data);
+          setFiltered(allPlaces.data);
         } catch (error) {
           console.log(error);
         }
     }
 
-    useEffect(() => { 
+    useEffect(() => {
         getPlaces();
       }, []);
 
 
     return(
-        <div className='row col-lg-8 mx-auto justify-content-center'>
-            {
-                places.map((place, i) => 
-                    <div key={i} >
-                        <PlaceCard place={place} />
-                    </div>)
-            }
-        </div>
+        <>
+        <Filter places={places} setFiltered={setFiltered} />
+            <div className='row col-lg-8 mx-auto justify-content-center'>
+                {
+                    filtered.map((place, i) => 
+                        <div key={i} >
+                            <PlaceCard place={place} />
+                        </div>)
+                }
+            </div>
+        </>
     )
 }
