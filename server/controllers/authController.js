40b5cbfd77147083f5a43a0d56957bc9b7f934b6
tugberkozkara <tugberkozkara@gmail.com>
2023-02-04 13:24:18 +0300

    Move auth logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,98 +1,40 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-import User from "../models/user.js";
+import AuthService from "../services/AuthService.js";
 
-dotenv.config();
-
-export default class AuthController{
+export default class AuthController {
 
     static getAllUsers = async (request, response) => {
-        try {
-            const allUsers = await User.find();
-            response.status(200).json(allUsers);
-        } catch (error) {
-            response.status(404).json({
-                message: error.message,
+        const allUsers = await AuthService.getAllUsers();
+        if(allUsers === null){
+            return response.status(404).json({
+                message: "No users found!",
             });
         }
+        return response.status(200).json(allUsers);
     };
-
-    static getUserByUsername = async (username) => {
-        try {
-            const user = await User.findOne({username: username});
-            return user;
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    
 
     static createUser = async (request, response) => {
         const {email, username, password} = request.body;
-        const isUserExists = await User.exists({email: email});
-        if(isUserExists){
+        const user = await AuthService.register(email, username, password);
+        if(user === null){
             return response.status(400).json({
-                message: "Email already registered!",
+                message: "User already exists!",
             });
         }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const lastLoginDate = Date.now();
-        const user = new User({
-            email: email,
-            username: username,
-            password: hashedPassword,
-            lastLoginDate: lastLoginDate
-        });
-
-        try {
-            await user.save();
-        } catch (error) {
-            return response.status(400).json({
-                message: error.message,
-            });
-        }
-
-        const token = jwt.sign({
-            username: user.username,
-            password: user.hashedPassword
-        },
-        process.env.SECRET_KEY,
-        {
-            expiresIn :"2h"
-        });
-
+        const token = AuthService.createToken(user.username, user.password);
         return response.status(201).json({
-            message: "Created successfully!",
+            message: "Register Successful!",
             token: token
         });
     };
 
-
     static loginUser = async (request, response) => {
         const {username, password} = request.body;
-        const user = await User.findOne({username: username});
-        const isPasswordCorrect = await bcrypt.compare(password, user?.password).catch(()=>{return false;});
-        if(!user || !isPasswordCorrect){
+        const token = await AuthService.login(username, password);
+        if(token === null){
             return response.status(401).json({
                 message: "Username or password is incorrect!",
             });
         }
-        const lastLoginDate = Date.now();
-        try {
-            await user.updateOne({lastLoginDate: lastLoginDate});
-        } catch (error) {
-            console.log(error.message);
-        }
-
-        const token = jwt.sign({
-            username: user.username,
-            password: user.password
-        },
-        process.env.SECRET_KEY,{
-            expiresIn :"2h"
-        });
-
         return response.status(200).json({
             message: "Login Successful!",
             token: token
---
+++ server/services/AuthService.js
@@ -0,0 +1,80 @@
+import User from '../models/User.js';
+
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default class AuthService {
+
+    static login = async (username, password) => {
+        const user = await AuthService.getUserByUsername(username);
+        if(user === null){
+            return null;
+        }
+        const isPasswordCorrect = bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return null;
+        }
+        const lastLoginDate = Date.now();
+        try {
+            await user.updateOne({lastLoginDate: lastLoginDate});
+        } catch (error) {
+            console.log(error.message);
+        }
+        const token = AuthService.createToken(user.username, user.password);
+        return token;
+    };
+
+    static register = async (email, username, password) => {
+        const isUserExists = await User.exists({email: email});
+        if(isUserExists){
+            return null;
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const lastLoginDate = Date.now();
+        const user = new User({
+            email: email,
+            username: username,
+            password: hashedPassword,
+            lastLoginDate: lastLoginDate
+        });
+
+        try {
+            await user.save();
+            return user;
+        } catch (error) {
+            return null;
+        }
+    };
+
+    static getAllUsers = async () => {
+        try {
+            const users = await User.find();
+            return users;
+        } catch (error) {
+            return null;
+        }
+    };
+
+    static getUserByUsername = async (username) => {
+        try {
+            const user = await User.findOne({username: username});
+            return user;
+        } catch (error) {
+            return null;
+        }
+    };
+
+    static createToken = (username, password) => {
+        const token = jwt.sign({
+            username: username,
+            password: password
+        },
+        process.env.SECRET_KEY, {
+            expiresIn :"2h"
+        });
+        return token;
+    };
+}
