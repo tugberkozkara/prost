4f0004da5023918c9a69c34a6bee9dba031ca682
tugberkozkara <tugberkozkara@gmail.com>
2023-02-20 13:58:59 +0300

    Remove api expression from endpoints

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const API_URL_LOGIN =  BASE_URL + "/api/auth/login";
-export const API_URL_REGISTER =  BASE_URL + "/api/auth/register";
-export const API_URL_PLACES = BASE_URL + "/api/places";
-export const API_URL_TAGS = BASE_URL + "/api/tags";
+export const API_URL_LOGIN =  BASE_URL + "/auth/login";
+export const API_URL_REGISTER =  BASE_URL + "/auth/register";
+export const API_URL_PLACES = BASE_URL + "/places";
+export const API_URL_TAGS = BASE_URL + "/tags";
