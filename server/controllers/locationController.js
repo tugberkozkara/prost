0b3b070bcf1c83cb24ee36d542ed2901e8872bb9
tugberkozkara <tugberkozkara@gmail.com>
2023-02-27 15:24:39 +0300

    Add location controllers

---
+++ server/controllers/locationController.js
@@ -0,0 +1,41 @@
+import LocationService from "../services/LocationService.js";
+
+export default class LocationController{
+
+    static getAllLocations = async (request, response) => {
+        const allLocations = await LocationService.getAll();
+        if(allLocations === null){
+            return response.status(404).json({
+                message: "No locations found!",
+            });
+        }
+        return response.status(200).json(allLocations);
+    };
+
+    static createLocation = async (request, response) => {
+        const { name } = request.body;
+        const location = await LocationService.create(name);
+        if(location === null){
+            return response.status(400).json({
+                message: "Location already exists!",
+            });
+        }
+        return response.status(201).json({
+            location: location,
+            message: "Created successfully!",
+        });
+    };
+
+    static deleteLocationById = async (request, response) => {
+        const { id } = request.params;
+        const isDeleted = await LocationService.deleteById(id);
+        if (!isDeleted) {
+            return response.status(404).json({
+                message: "Not found!",
+            });
+        }
+        return response.status(200).json({
+            message: "Deleted successfully!",
+        });
+    };
+}
