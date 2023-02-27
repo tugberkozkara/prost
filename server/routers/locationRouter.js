92ab25942b9057c926250fd752e26e40652bb3b9
tugberkozkara <tugberkozkara@gmail.com>
2023-02-28 00:55:34 +0300

    Add auth check to tag and location endpoints

--- server/routers/locationRouter.js
+++ server/routers/locationRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import LocationController from "../controllers/locationController.js";
+import AuthHandler from "../handlers/authHandler.js";
 import IDHandler from "../handlers/idHandler.js";
 
 var router = express.Router();
 var jsonParser = bodyParser.json();
 
 router.get("/", LocationController.getAllLocations);
-router.post("/", jsonParser, LocationController.createLocation);
-router.delete("/:id", IDHandler.getIdHandler, LocationController.deleteLocationById);
+router.post("/", jsonParser, AuthHandler.checkAuth, LocationController.createLocation);
+router.delete("/:id", AuthHandler.checkAuth, IDHandler.getIdHandler, LocationController.deleteLocationById);
 
 export default router;
--- server/routers/tagRouter.js
+++ server/routers/tagRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import TagController from "../controllers/tagController.js";
+import AuthHandler from "../handlers/authHandler.js";
 import IDHandler from "../handlers/idHandler.js";
 
 var router = express.Router();
 var jsonParser = bodyParser.json();
 
 router.get("/", TagController.getAllTags);
-router.post("/", jsonParser, TagController.createTag);
-router.delete("/:id", IDHandler.getIdHandler,TagController.deleteTagById);
+router.post("/", jsonParser, AuthHandler.checkAuth, TagController.createTag);
+router.delete("/:id", AuthHandler.checkAuth, IDHandler.getIdHandler,TagController.deleteTagById);
 
 export default router;
