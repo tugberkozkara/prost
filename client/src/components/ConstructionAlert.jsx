ca6c12620d6645551ee48984024fa8a80ed184fa
tugberkozkara <tugberkozkara@gmail.com>
2023-09-06 13:49:25 +0300

    Show alert instead of navigating

--- client/src/components/ConstructionAlert.jsx
+++ client/src/components/ConstructionAlert.jsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import underConstrImg from '../media/503.png';
 
-const Construction = () => {
+const ConstructionAlert = () => {
   return (
     <div className='row col-10 col-sm-8 col-md-6 col-lg-6 col-xl-4 mx-auto my-0 justify-content-center'>
         <div className="card align-items-center border-white">
-            <img src={underConstrImg} className="card-img-top rounded col-lg-8" />
+            <img src={underConstrImg} alt="Construction" className="card-img-top rounded col-lg-8" />
             <div className="card-body p-5 text-center">
                 <h5 className="card-title mx-auto">Prost! is under construction.</h5>
                 <h6 className="card-subtitle my-2 text-muted">We'll be back soon!</h6>
@@ -14,4 +15,4 @@ const Construction = () => {
   )
 }
 
-export default Construction
+export default ConstructionAlert
--- client/src/pages/ListPlaces.js
+++ client/src/pages/ListPlaces.js
@@ -1,20 +1,20 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from "react-router-dom";
 import PlaceCard from '../components/PlaceCard';
 import Filter from '../components/Filter';
 import ProfileHeader from '../components/ProfileHeader';
+import ConstructionAlert from '../components/ConstructionAlert';
 import { API_URL_PLACES } from '../utils/constants';
-import axios from 'axios';
-import { useParams, useNavigate } from "react-router-dom";
 
 
 export default function ListPlaces(){
     const [places, setPlaces] = useState([]);
     const [filtered, setFiltered] = useState([]);
-
     const [activeTags, setActiveTags] = useState([]);
     const [activeLocations, setActiveLocations] = useState([]);
+    const [networkErrorAlert, setNetworkErrorAlert] = useState(false);
 
-    const navigate = useNavigate();
     const params = useParams(null);
 
     const getPlaces = async () => {
@@ -29,7 +29,7 @@ export default function ListPlaces(){
             setFiltered(allPlaces.data);
         } catch (error) {
             if (error.message === "Network Error") {
-                navigate("/503");
+                setNetworkErrorAlert(true);
             }
         }
     }
@@ -49,32 +49,40 @@ export default function ListPlaces(){
 
     return(
         <>
-        {params?.username ? (
-            <ProfileHeader username={params?.username} places={places} />
-        ):(
-        <></>)}
-        
-        <Filter places={places} filtered={filtered} setFiltered={setFiltered} activeTags={activeTags} setActiveTags={setActiveTags} activeLocations={activeLocations} setActiveLocations={setActiveLocations} />
-            <div className='row col-lg-8 col-xl-8 mx-auto justify-content-center'>
-                {filtered.length !== 0 ? (
-                    filtered.map((place, i) => 
-                        <div key={i} className="w-auto">
-                            <PlaceCard place={place} filtered={filtered} setFiltered={setFiltered} />
-                        </div>)
+            {networkErrorAlert ?
+                (
+                    <ConstructionAlert />
                 ):
                 (
-                    <div className="card my-5 px-5">
-                        <div className="card-body p-5 text-center">
-                            <h5 className="card-title mx-auto">OOPS...</h5>
-                            <h6 className="card-subtitle my-2 text-muted">No places found!</h6>
-                            <button type="button" className="btn btn-outline-primary mx-auto mt-3" onClick={clearFilters}>Clear Filters</button>
+                    <>
+                        {params?.username && 
+                            <ProfileHeader username={params?.username} places={places} />
+                        }
+                        
+                        <Filter places={places} filtered={filtered} setFiltered={setFiltered} activeTags={activeTags} setActiveTags={setActiveTags} activeLocations={activeLocations} setActiveLocations={setActiveLocations} />
+                        <div className='row col-lg-8 col-xl-8 mx-auto justify-content-center'>
+                            {filtered.length !== 0 ? (
+                                filtered.map((place, i) => 
+                                    <div key={i} className="w-auto">
+                                        <PlaceCard place={place} filtered={filtered} setFiltered={setFiltered} />
+                                    </div>)
+                            ):
+                            (
+                                <div className="card my-5 px-5">
+                                    <div className="card-body p-5 text-center">
+                                        <h5 className="card-title mx-auto">OOPS...</h5>
+                                        <h6 className="card-subtitle my-2 text-muted">No places found!</h6>
+                                        <button type="button" className="btn btn-outline-primary mx-auto mt-3" onClick={clearFilters}>Clear Filters</button>
+                                    </div>
+                                </div>
+                            )}
+                            <div className="text-center">
+                                <p className="text-muted small">Icons made by <a className="text-decoration-none" href="https://www.flaticon.com/authors/freepik">Freepik</a> from <a className="text-decoration-none" href="https://www.flaticon.com">www.flaticon.com</a>.</p>
+                            </div>
                         </div>
-                    </div>
-                )}
-                <div className="text-center">
-                    <p className="text-muted small">Icons made by <a className="text-decoration-none" href="https://www.flaticon.com/authors/freepik">Freepik</a> from <a className="text-decoration-none" href="https://www.flaticon.com">www.flaticon.com</a>.</p>
-                </div>
-            </div>
+                    </>
+                )
+            }
         </>
     )
 }
