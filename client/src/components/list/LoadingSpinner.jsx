df73e35f505ebce6ba7ea718a0622ff4c3df1380
tugberkozkara <tugberkozkara@gmail.com>
2023-09-11 22:33:35 +0300

    Update loading icon

--- client/src/components/list/LoadingSpinner.jsx
+++ client/src/components/list/LoadingSpinner.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import beerLoading from '../../media/beer-loading.gif';
 
 const LoadingSpinner = () => {
   return (
     <>
         <div className="container mb-0 mt-5 text-center">
-                <div className="spinner-border" role="status"></div>
-                <p className="mb-0 small text-muted">Loading...</p>
+                <img src={beerLoading} alt="beer-loading" className="col-3 col-md-2"/>
+                <p className="mb-0 text-muted">Loading...</p>
         </div>
     </>
   )
